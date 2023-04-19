dbd74039e3d99734717664729bda8aee3c8df04a
Ahmad Reza Enshaee <enshaee.ahmadreza@gmail.com>
2023-04-19 16:29:35 +0400

    added day 6 files.

---
+++ src/main/kotlin/org/example/aoc2022/Day6.kt
@@ -0,0 +1,17 @@
+package org.example.aoc2022
+
+fun main() {
+	println("Day1: ${Day6.runDay6Part1("Day6Input")}")
+	println("Day2: ${Day6.runDay6Part2("Day6Input")}")
+}
+
+object Day6 {
+
+	fun runDay6Part1(fileName: String): String {
+		TODO()
+	}
+
+	fun runDay6Part2(fileName: String): String {
+		TODO()
+	}
+}
---
+++ src/test/kotlin/org/example/aoc2022/Day6KtTest.kt
@@ -0,0 +1,16 @@
+package org.example.aoc2022
+
+import org.junit.jupiter.api.Test
+
+internal class Day6KtTest {
+
+	@Test
+	fun runDay6Part1() {
+		TODO()
+	}
+	@Test
+	fun runDay6Part2() {
+		TODO()
+	}
+
+}
