adebd63f7afb8d3e7314b571871e266704ba0720
Ahmad Reza Enshaee <enshaee.ahmadreza@gmail.com>
2023-04-19 16:29:10 +0400

    corrected the test names.

--- src/test/kotlin/org/example/aoc2022/Day5KtTest.kt
+++ src/test/kotlin/org/example/aoc2022/Day5KtTest.kt
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test
 internal class Day5KtTest {
 
 	@Test
-	fun runDay4Part1() {
+	fun runDay5Part1() {
 		assertEquals("CMZ", Day5.runDay5Part1("Day5InputTest"))
 	}
 	@Test
-	fun runDay4Part2() {
+	fun runDay5Part2() {
 		assertEquals("MCD", Day5.runDay5Part2("Day5InputTest"))
 	}
 
